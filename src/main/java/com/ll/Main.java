dc8f1870294ac8b8a3768badf35148f5211873c0
hyo-jung <rlagywnd05@gmail.com>
2025-07-13 18:54:36 +0900

    클래스구조변경

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,5 +1,6 @@
 package com.ll;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -7,37 +8,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("==게시판 앱==");
-        String inStr="등록";
-        int i =1;
-        StringBuilder sb = new StringBuilder();
-        while (true) {
-            try {
-                inStr = sc.nextLine().trim();
-                if(inStr.equals("종료")){
-                    break;
-                }
-                else if(inStr.equals("등록") ){
-                    System.out.print("제목 등록해주세요: ");
-                    inStr = sc.nextLine().trim();
-                    sb.append(inStr);
-                    System.out.print("내용 등록해주세요: ");
-                    inStr = sc.nextLine().trim();
-                    sb.append(inStr);
-                    System.out.println(i + "번 게시물 등록되었습니다");
-                    i++;
-                }
-
-            } catch (InputMismatchException e) {
-                System.out.println("오류");
-                sc.nextLine(); // 버퍼 비우기
-            }
-        }
-        System.out.println("등록된 문자열은 : " + sb.toString() );
-
-
-
-        sc.close();
+//        app App = new app(sc);
+//        App.run();
+        new app(sc).run();
 
     }
 }
